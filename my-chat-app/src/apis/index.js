7c7bbad23b993e96729a766ce61f9c91c54c602a
Alfian Rudi Yogatama <alvmarjinal@gmail.com>
2021-09-23 07:06:59 +0700

    change http url deployedheroku server

--- my-chat-app/src/apis/index.js
+++ my-chat-app/src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-	baseURL: "http://localhost:3000",
+	baseURL: "https://ayochat.herokuapp.com",
 	headers: {
 		access_token: localStorage.getItem('access_token'),
 	},
